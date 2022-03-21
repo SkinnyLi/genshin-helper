b9ceb09362611e81fd2398c608b08ad4ec8eca5a
SkinnyLi <97529315+SkinnyLi@users.noreply.github.com>
2022-03-21 10:09:47 +0800

    Add files via upload

---
+++ genshin-dailynote-helper-main/genshin-dailynote-helper-main/app.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const utils = require('./lib/utils.js');
+const logger = require('./lib/logger.js');
+const refPlayer = require('./lib/mihoyo/refPlayer.js');
+const getDailyNote = require('./lib/mihoyo/getDailyNote.js');
+
+const config = require('./config.json');
+const e = require('express');
+
+var cache;
+require('./lib/global').init();
+
+const init = async function() {
+
+	if(!fs.existsSync('./cache.json')) {
+		fs.writeFileSync('./cache.json',"{}");
+	}
+	cache = require('./cache.json');
+
+	if (cache['region'] == undefined || cache['game_uid'] == undefined) {
+		logger.i("缓存中未包含玩家信息，正在尝试获取...");
+		await refPlayer();
+	}
+	reloadCache();
+	while (cache['game_uid'] == undefined) {
+		logger.i('读取缓存失败，等待缓存写出到文件...');
+		await utils.randomSleepAsync();
+		reloadCache();
+	}
+	app.get('/resin',async function (req, res) {
+		logger.i('[Get] /resin');
+		if (cache['last_update'] == undefined || Math.floor(Date.now() / 1000) - cache['last_update'] > config.cache_time) {
+			logger.i('本地缓存未找到或已过期，从远程更新数据中...');
+			await getDailyNote(cache['region'],cache['game_uid']);
+		} else {
+			logger.i('从本地缓存发送数据...');
+		}
+		genFormatedResponse(res);
+	});
+
+	app.get('/resin/all',async function(req,res) {
+		logger.i('[Get] /resin/all');
+		if (cache['last_update'] == undefined || Math.floor(Date.now() / 1000) - cache['last_update'] > config.cache_time) {
+			logger.i('本地缓存未找到或已过期，从远程更新数据中...');
+			await getDailyNote(cache['region'],cache['game_uid']);
+		} else {
+			logger.i('从本地缓存发送数据...');
+		}
+		res.status(200);
+		reloadCache();
+		res.send(cache);
+		logger.i('数据已发送');
+	});
+
+	app.get('/resin/force_refresh',async function(req,res) {
+		logger.i('[Get] /resin/force_refresh');
+		logger.i('开始强制刷新缓存...');
+		await getDailyNote(cache['region'],cache['game_uid']);
+		res.status(200);
+		reloadCache();
+		res.send('{"msg":"OK"}');
+		logger.i('强制刷新完成');
+	});
+
+
+	app.listen(config.port, '0.0.0.0', function () {
+		logger.i('初始化完成！开始在端口 ' + config.port + ' 监听请求！')
+	});
+}
+
+function reloadCache() {
+	// logger.i("重新载入缓存...");
+	delete require.cache[require.resolve('./cache.json')];
+	cache = require('./cache.json');
+}
+
+function genFormatedResponse(res) {
+	var result = {};
+	reloadCache();
+	result['resin'] = cache['current_resin'] + ' / ' + cache['max_resin'];
+	if (parseInt(cache['resin_recovery_time']) > 0) {
+		var recHour = parseInt(cache['resin_recovery_time'] / 60 / 60);
+		var recMin = parseInt((cache['resin_recovery_time'] - recHour * 60 * 60) / 60);
+		var recSec = cache['resin_recovery_time'] - recHour *60 * 60 - recMin * 60;
+		result['resin'] = result['resin'] + ' (' +recHour+'h '+recMin+"m "+recSec+"s)";
+	}
+	result['task'] = cache['finished_task_num'] + ' / ' + cache['total_task_num'];
+	if (cache['finished_task_num'] == cache['total_task_num'] ){
+		if (cache['is_extra_task_reward_received']) {
+			result['task'] = result['task'] + '  (额外奖励已领取)';
+		} else {
+			result['task'] = result['task'] + '  (额外奖励未领取)';
+		}
+	}
+	result['expedition'] = cache['finished_expedition_num'] + ' / ' + cache['current_expedition_num'] +  ' / ' + cache['max_expedition_num'];
+	var max_expedition_time = 0;
+	if (cache['finished_expedition_num'] != cache['current_expedition_num']) {	
+		cache.expeditions.forEach(element => {
+			if (element.status != 'Finished') {
+				var remained_time = parseInt(element.remained_time);
+				if (remained_time > max_expedition_time) {
+					max_expedition_time = remained_time;
+				}
+			}
+		});
+		var expHour = parseInt(max_expedition_time / 60 / 60);
+		max_expedition_time = max_expedition_time - expHour * 60 * 60;
+		var expMin = parseInt(max_expedition_time / 60);
+		max_expedition_time = max_expedition_time - expMin * 60;
+		result['expedition'] = result['expedition'] + ' (' + expHour + 'h ' + expMin + 'm ' + max_expedition_time + 's)'; 
+	}
+	result['last_update'] = cache['last_update_format'];
+	res.status(200);
+	res.send(JSON.stringify(result));
+	logger.i('数据已发送');
+}
+
+init();
---
+++ genshin-dailynote-helper-main/genshin-dailynote-helper-main/lib/global.js
@@ -0,0 +1,63 @@
+const _ = require('lodash');
+const utils = require('./utils');
+const md5 = require('md5');
+const config = require('./../config.json')
+const APP_VERSION = "2.2.0";
+const DEVICE_ID = utils.randomString(32).toUpperCase();
+const DEVICE_NAME = utils.randomString(_.random(1, 10));
+
+const init = () => {
+  global.getHeaderOld = () => {
+    let randomStr = utils.randomString(6);
+    let timestamp = Math.floor(Date.now() / 1000)
+  
+    // iOS sign
+    let sign = md5(`salt=b253c83ab2609b1b600eddfe974df47b&t=${timestamp}&r=${randomStr}`);
+
+    return {
+      'Cookie': config.cookie,
+      'Content-Type': 'application/json',
+      'User-Agent': 'Hyperion/67 CFNetwork/1128.0.1 Darwin/19.6.0',
+      'Referer': 'https://app.mihoyo.com',
+      'x-rpc-channel': 'appstore',
+      'x-rpc-device_id': DEVICE_ID,
+      'x-rpc-app_version': APP_VERSION,
+      'x-rpc-device_model': 'iPhone11,8',
+      'x-rpc-device_name': DEVICE_NAME,
+      'x-rpc-client_type': '1', // 1 - iOS, 2 - Android, 4 - Web
+      'DS': `${timestamp},${randomStr},${sign}`
+      // 'DS': `1602569298,k0xfEh,07f4545f5d88eac59cb1257aef74a570`
+    }
+  }
+  global.getHeader = (URL) => {
+    let randomStr = utils.randomString(6);
+    let timestamp = Math.floor(Date.now() / 1000);
+    let queryStr = '';
+    if (URL.split('?').length > 1) {
+      queryStr = URL.split('?')[1];
+    }
+    // iOS sign
+    let sign = md5(`salt=xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs&t=${timestamp}&r=${randomStr}&b=&q=${queryStr}`);
+
+    return {
+      'Host': 'api-takumi.mihoyo.com',
+      'Connection': 'keep-alive',
+      'Accept': 'application/json, text/plain, */*',
+      'Origin': 'https://webstatic.mihoyo.com',
+      'x-rpc-app_version': '2.12.1',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; MI 6 Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.158 Mobile Safari/537.36 miHoYoBBS/2.12.1',
+      'x-rpc-client_type': 5,
+      'Referer': 'https://webstatic.mihoyo.com/app/community-game-records/index.html?v=6',
+      'Accept-Encoding': 'gzip, deflate',
+      'Accept-Language': 'zh-CN,en-US;q=0.9',
+      'X-Requested-With': 'com.mihoyo.hyperion',
+      'Cookie': config.cookie,
+      'Content-Type': 'application/json',
+      'DS': `${timestamp},${randomStr},${sign}`
+    }
+  }
+}
+
+module.exports = {
+  init
+}
